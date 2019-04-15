38cd0cd50b50226c6002af06103ca49404e2e78a
sean-hanlon <c15801977@mydit.ie>
2019-04-15 19:17:53 +0100

    tweaked login functionality so that it worked

--- store/src/main/java/REST/store/resource/UserResource.java
+++ store/src/main/java/REST/store/resource/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -25,19 +26,23 @@ public class UserResource {
 	public List<User> getUsers() {
 		return userService.getAllUsers();
 	}
-	
+
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public void addUser(User user) {
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Path("/addUser")
+	public void addUser(@FormParam(value = "name") String name, @FormParam(value = "email") String email,
+			@FormParam(value = "password") String password, @FormParam(value = "address") String address,
+			@FormParam(value = "payMethod") String payMethod) {
+		User user = new User(name, email, password, address, payMethod);
 		userService.addUser(user);
 	}
 	
 	@Context
 	private HttpServletRequest request;
 	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Path("/login")
-	public void login(String email, String password) {
+	public void login(@FormParam(value = "email") String email, @FormParam(value = "password") String password) {
 		User user = userService.loginUser(email, password);
 		
 		if (user == null)
@@ -48,7 +53,16 @@ public class UserResource {
 		{
 			request.getSession().setAttribute("user", user);
 			request.setAttribute("customer", user);
+			request.getSession().setAttribute("name", user.getId());
+			System.out.println(request.getSession().getAttribute("name")+" is logged in");
 		}
 	}
-
+	
+	@POST
+	@Path("/logout")
+	public void logout() {
+		request.getSession().setAttribute("user", null);
+		request.setAttribute("customer", null);
+	}
+	
 }
